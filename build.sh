c48b24b28aa68a8fcbf76fbf178552c672b70a5e
beos_zealot <beos.zealot@gmail.com>
2013-11-03 16:41:44 +0200

    fixed destination path for bsl and rez binaries

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 cd bsl
 make
-cp bsl /boot/home/non-packaged/bin
+cp bsl /boot/home/config/non-packaged/bin
 cd ../rez
 make
-cp rez /boot/home/non-packaged/bin
+cp rez /boot/home/config/non-packaged/bin
 cd ../sum-it
 make
 
